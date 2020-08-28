f1fa14ffc2f02cf690b17d8ed41de5966b011332
MariaSierra <66576032+mariasierralizarazo@users.noreply.github.com>
2020-08-28 14:10:06 -0400

    Making calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,37 @@
 import os 
 import csv
+total_amount = 0
+total_months = 0
+average = 0
+
 path_file = os.path.join('Resources','budget_data.csv')
 #print(f'This is the path file {path_file}')
-with open(path_file, newline='') as csvfile:
-    reader=csv.reader(csvfile,delimiter=',')
-    file_header=next(reader)
-    #print(f'File header: {file_header}')
+
+with open(path_file,newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    # next skip the row - header
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        total_amount = total_amount + float(row[1])
+        total_months += 1
+        average = float(row[1])-average
+#average = average/total_months
+
+# with open(path_file) as csvfile:
+#     file_reader=csv.reader(csvfile,delimiter=',')
+#     file_header=next(file_reader)
+#     print(f'File header: {file_header}')
+#     months_num = len(lis`t(file_reader))
+#     for row in file_reader:
+#         print(row)
+#         print('Aqui')
+        #total_amount = total_amount + row[1]
+print("\n Financial Analysis")
+print("------------------------------------------")
+print(f'Total Months: {total_months}')
+print(f'Total: {total_amount}')
+print(f'Average Change: {average}')
+print("------------------------------------------\n")    
