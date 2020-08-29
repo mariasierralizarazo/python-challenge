7d3e0b1538a6e9c97655dd8e22103295d8ab9b35
MariaSierra <66576032+mariasierralizarazo@users.noreply.github.com>
2020-08-28 23:39:31 -0400

    .txt file exportation and final comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,7 +14,9 @@ with open(path_file,newline='') as csvfile:
     csv_header = next(csvreader)
     # List cretion with file's information
     list_file = list(csvreader)
+# The number of votes will be equal to the number of rows in the file or the number of items in the list
 total_votes = len(list_file)
+# Loop for candidates identification and adding each numbers of votes 
 for item in list_file:
     exist = False
     for i in range(len(results)):
@@ -26,14 +28,18 @@ for item in list_file:
     if exist == False:
        results.append([item[2],1])
 
+L = "Election Results\n" + "------------------------------------------\n"+ 'Total Votes: ' + str(total_votes) + "\n------------------------------------------\n"
+
 # Terminal printing
 print("\nElection Results")
 print("------------------------------------------")
 print(f'Total Votes: {total_votes}')
 print("------------------------------------------")
+# Printing loop and winner name identification 
 for item in results:
     percentage = (item[1]*100)/total_votes
     print(f'{item[0]}: {"%.3f" % percentage}% ({item[1]})')
+    L = L + str(item[0])+ ': '+ str("%.3f" % percentage) + '% (' + str(item[1]) + ')'+'\n'
     counter = counter+1
     if counter==1:
         winner = item[0]
@@ -44,3 +50,9 @@ for item in results:
 print("------------------------------------------") 
 print(f'Winner: {winner}')   
 print("------------------------------------------") 
+
+# .txt file exportation 
+L = L + "------------------------------------------\n" + 'Winner:' + winner +'\n------------------------------------------'
+txt_file_path = os.path.join('Resources','Output_file.txt')
+with open(txt_file_path, "w") as txt_file:
+    txt_file.write(L)
