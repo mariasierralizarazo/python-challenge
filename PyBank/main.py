0961143dd93f2d2fbb2cfbe4bd486ec799d32c3b
MariaSierra <66576032+mariasierralizarazo@users.noreply.github.com>
2020-08-28 21:08:44 -0400

    Calculating final results and printing the analysis to the terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,13 +2,10 @@ import os
 import csv
 total_amount = 0
 total_months = 0
-average = 0
-counter = 2
-greatest_profits = []
-greatest_losses = []
+profit_diff = 0
+losses_diff = 0
 
 path_file = os.path.join('Resources','budget_data.csv')
-#print(f'This is the path file {path_file}')
 
 with open(path_file,newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -17,28 +14,28 @@ with open(path_file,newline='') as csvfile:
     list_file = list(csvreader)
 total_months = len(list_file)
 average = (float(list_file[total_months-1][1])-float(list_file[0][1]))/(total_months-1)
-for item in list_file:
-    total_amount= total_amount+ float(item[1])
+for i in range(total_months-1):
+    total_amount= total_amount+ float(list_file[i][1])
+    if (i==0):
+        profit_diff = float(list_file[i+1][1])-float(list_file[i][1])
+        greatest_profits = list_file[i+1][0]
+    elif ((float(list_file[i+1][1])-float(list_file[i][1]))> profit_diff):
+        profit_diff = float(list_file[i+1][1])-float(list_file[i][1])
+        greatest_profits = list_file[i+1][0]
+
+    if (i==0):
+        losses_diff = float(list_file[i+1][1])-float(list_file[i][1])
+        greatest_losses = list_file[i+1][0]
+    elif((float(list_file[i+1][1])-float(list_file[i][1]))< losses_diff):
+        losses_diff = float(list_file[i+1][1])-float(list_file[i][1])
+        greatest_losses = list_file[i+1][0]
 
   
-    
-    #average = list_file{}
-
-
-#average = average/total_months
-
-# with open(path_file) as csvfile:
-#     file_reader=csv.reader(csvfile,delimiter=',')
-#     file_header=next(file_reader)
-#     print(f'File header: {file_header}')
-#     months_num = len(lis`t(file_reader))
-#     for row in file_reader:
-#         print(row)
-#         print('Aqui')
-        #total_amount = total_amount + row[1]
 print("\n Financial Analysis")
 print("------------------------------------------")
 print(f'Total Months: {total_months}')
 print(f'Total: {total_amount}')
 print(f'Average Change: {"%.2f" % average}')
+print(f'Greatest Increase in Profits: {greatest_profits} ($ {profit_diff})')
+print(f'Greatest Decrease in Profits: {greatest_losses} ($ {losses_diff})')
 print("------------------------------------------\n")    
